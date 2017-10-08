623434174ee3b8a64bf79beb72b1297c791e28a9
Swapnil Kumar <swapnil.kumar@blackbuck.com>
2017-10-08 12:03:00 +0530

    all and first non repeating characters from stream

---
+++ String/all_non_repeating_character_stream_characters.py
@@ -0,0 +1,16 @@
+def all_non_repeating_character_stream_characters(str):
+    dll = []
+    count = [0] * 256
+
+    for s in str:
+        count[ord(s)] += 1
+        if count[ord(s)] > 1:
+            if s in dll:
+                dll.remove(s)
+        elif count[ord(s)] == 1:
+            dll.append(s)
+
+    print dll
+
+
+all_non_repeating_character_stream_characters('ogeekqusigefeksazndgeeksr')
---
+++ String/first_non_repeating_character_stream_characters.py
@@ -0,0 +1,18 @@
+def first_non_repeating_character_stream_characters(str):
+    inDLL = []
+    repeated = [0] * 256
+    i = 0
+    for s in str:
+        repeated[ord(s)] += 1
+        if repeated[ord(s)] >= 1:
+            if s not in inDLL:
+                inDLL.append(s)
+            else:
+                inDLL.remove(s)
+
+        if i == len(str) - 1:
+            print "First non repeating character is " + inDLL[0]
+        i += 1
+
+
+first_non_repeating_character_stream_characters('geeksforgeeksandgeeksquizfor')
